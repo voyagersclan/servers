8a4ea6773b98bf155cb9f31ccdbc1bfe401fa5ff
Cody Garrett <cody@garrett.ms>
2020-02-05 15:55:56 -0500

    added udp

--- run/run_garrys_mod.sh
+++ run/run_garrys_mod.sh
@@ -9,6 +9,8 @@ STEAM_PORT_MAPPING="27015:27015"
 # docker stop $CONTAINER_NAME
 # docker rm $CONTAINER_NAME
 
+#-p 8080:80/tcp -p 8080:80/udp	
+
 #Use below to test and look at file structure
 #docker run -it --rm voyagers:garrys_mod bash
 
@@ -17,10 +19,10 @@ if [ ! "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
     #Logic to detect if this is a windows or linux box
     if [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
         #Not use mounts if on windows box
-        docker run -it --name $CONTAINER_NAME -p "$STEAM_PORT_MAPPING" $IMAGE_NAME
+        docker run -it --name $CONTAINER_NAME -p "$STEAM_PORT_MAPPING" -p "$STEAM_PORT_MAPPING/tcp" -p "$STEAM_PORT_MAPPING/udp" $IMAGE_NAME
     else
         #Use Mounts if on Linux Box
-        docker run -it -v /opt/mounts:/opt/garrys_mod/mounts --name $CONTAINER_NAME -p "$STEAM_PORT_MAPPING" $IMAGE_NAME
+        docker run -it -v /opt/mounts:/opt/garrys_mod/mounts --name $CONTAINER_NAME -p "$STEAM_PORT_MAPPING/tcp" -p "$STEAM_PORT_MAPPING/udp" $IMAGE_NAME
     fi
 fi
 
