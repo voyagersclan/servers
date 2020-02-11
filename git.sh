738cf6c8a6b3865ba6a2f1b8b40b071f65eff83f
Cody Garrett <cody@garrett.ms>
2020-02-11 12:10:43 -0500

    made all scripts executable

--- git.sh
+++ git.sh
@@ -3,6 +3,7 @@
 remoteOrigin="https://github.com/voyagersclan/servers.git"
 
 if [ "$1" = "push" ]; then
+    git update-index --chmod=+x ./*/*.sh
     git add -A
     git add *
     git remote add origin "$remoteOrigin"
